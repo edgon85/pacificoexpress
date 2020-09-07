76471b3e19335aae4917d7e0fbbafefa42c3a8d6
Edwin González <hgedwin@wedevgt.com>
2020-09-07 00:55:26 -0600

    seccion de gps implemtado con imagen

--- src/app/pages/gps/gps.module.ts
+++ src/app/pages/gps/gps.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { GpsRoutingModule } from './gps-routing.module';
 import { GpsComponent } from './components/gps/gps.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [GpsComponent],
   imports: [
     CommonModule,
-    GpsRoutingModule
+    GpsRoutingModule,
+    SharedModule
   ]
 })
 export class GpsModule { }
