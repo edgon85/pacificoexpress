3fb088599d24a8004105bcd3284b32ce3782f36f
edgon <edgon85@gmail.com>
2020-09-27 12:02:48 -0600

    implementando routeroutlet en vehiculos

---
+++ src/app/pages/vehiculos/components/vehiculos-layout/vehiculos-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehiculos-layout',
+  templateUrl: './vehiculos-layout.component.html',
+  styleUrls: ['./vehiculos-layout.component.scss']
+})
+export class VehiculosLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/vehiculos/vehiculos.module.ts
+++ src/app/pages/vehiculos/vehiculos.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { VehiculosRoutingModule } from './vehiculos-routing.module';
 import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { VehiculosLayoutComponent } from './components/vehiculos-layout/vehiculos-layout.component';
 
 
 @NgModule({
-  declarations: [VehiculosComponent],
+  declarations: [VehiculosComponent, VehiculosLayoutComponent],
   imports: [
     CommonModule,
     VehiculosRoutingModule,
